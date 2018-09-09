cf5373cbf843a699a75683cd92772381215ac092
Tom Fenyak <tjf0027@tigermail.auburn.edu>
2018-09-09 15:42:09 -0400

    Added MenuStrip with File, Tools, and About items with their sub - items

--- WebBrowserUI/TigerNet.cs
+++ WebBrowserUI/TigerNet.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WebBrowserUI
 {
-     public partial class Form1 : Form
+     public partial class TigerNet : Form
      {
-          public Form1()
+          public TigerNet()
           {
                InitializeComponent();
           }
